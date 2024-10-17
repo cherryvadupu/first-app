779dc4b85b0eaeb333f518a61e734067d175e631
cherryvadupu <153109025+cherryvadupu@users.noreply.github.com>
2024-10-18 00:33:56 +0530

    Update App.java

--- src/main/java/info/phani/App.java
+++ src/main/java/info/phani/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        for(int i=1;i<=10;i++){
+            if(i!=0){
+            sSystem.out.println("numbers "+i);
+            }
+        }
     }
 }
